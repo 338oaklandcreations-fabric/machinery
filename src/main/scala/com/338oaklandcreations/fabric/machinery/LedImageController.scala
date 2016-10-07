a2c66361c8bdcbbaf36a156ddafad14a172f771f
Mauricio Bustos <m@bustos.org>
2016-10-07 00:23:50 -0700

    initial windflowers deploy

--- src/main/scala/com/338oaklandcreations/fabric/machinery/LedImageController.scala
+++ src/main/scala/com/338oaklandcreations/fabric/machinery/LedImageController.scala
@@ -44,7 +44,7 @@ object LedImageController extends HostActor with HostAware {
 
   val ConnectionTickInterval = 5 seconds
   val TickInterval = {
-    24 milliseconds
+    200 milliseconds
     //if (windflowersHost) 1000 milliseconds
     //else 12 milliseconds
   }
