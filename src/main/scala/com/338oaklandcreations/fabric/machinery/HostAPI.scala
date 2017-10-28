7c4fc28bfd69ed0ae2f454848945d98d65a35c3e
Mauricio Bustos <m@bustos.org>
2017-10-27 21:07:33 -0700

    machinery

--- src/main/scala/com/338oaklandcreations/fabric/machinery/HostAPI.scala
+++ src/main/scala/com/338oaklandcreations/fabric/machinery/HostAPI.scala
@@ -179,6 +179,7 @@ class HostAPI extends Actor with ActorLogging with HostActor {
         if (wellLightTickScheduler != null) wellLightTickScheduler.cancel
         wellLightTickScheduler = null
       }
+      
     case WellLightSettingsRequest =>
       context.sender ! WellLightSettings(wellLightSettings.powerOn, pwmCommandLevel(wellLightSettings.level))
     case Reboot => CommandResult(Process("sudo reboot").!)
